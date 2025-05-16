49dafdbd50bc4f6f10f6dd4923409d421918a756
sruthisridhar25 <sruthisridhar25@gmail.com>
2025-05-16 16:33:22 -0700

    anonymized student names

---
+++ scripts/student_IDs.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import hashlib
+
+df = pd.read_csv("scripts/extracted_by_person_filtered.csv")
+
+def generate_student_id(name, index):
+    return 'S' + hashlib.sha1(f'{name}_{index}'.encode()).hexdigest()[:8]
+
+df['student_id'] = [generate_student_id(name, idx) for idx, name in enumerate(df['person_name'])]
+df = df.drop(columns=['person_name'])
+df = df[['student_id'] + [col for col in df.columns if col != 'student_id']]
+
+df.to_csv('anonymized_students.csv', index=False)

c669e68430708b34869e5d869df7affb0589bbfa
Caitlin Chan <cdechan@ucdavis.edu>
2025-06-02 19:29:55 -0700

    anonymize added positive test cases, add to master

--- scripts/student_IDs.py
+++ scripts/student_IDs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import hashlib
 
-df = pd.read_csv("scripts/negative_data.csv")
+df = pd.read_csv("updated_extracted_by_person_filtered.csv")
 
 def generate_student_id(name, index):
     return 'S' + hashlib.sha1(f'{name}_{index}'.encode()).hexdigest()[:8]
@@ -10,4 +10,4 @@ df['student_id'] = [generate_student_id(name, idx) for idx, name in enumerate(df
 df = df.drop(columns=['person_name'])
 df = df[['student_id'] + [col for col in df.columns if col != 'student_id']]
 
-df.to_csv('anonymized_negative.csv', index=False)
+df.to_csv('data/added_positive_data.csv', index=False)
